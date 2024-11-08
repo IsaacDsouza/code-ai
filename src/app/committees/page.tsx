76ffdaaa32c42fe73830ee57a9e3a79ca9f57487
IsaacDsouza <isaacmdsouza@gmail.com>
2024-11-08 16:04:08 +0530

    Committee page

---
+++ src/app/committees/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Committees = () => {
+    return (
+        <div>
+            <h1>Welcome to the Awards Page</h1>
+        </div>
+    );
+};
+
+export default Committees;
