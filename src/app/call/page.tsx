c898e519d3b7c131fdc72902c695d9bb142b4287
IsaacDsouza <isaacmdsouza@gmail.com>
2024-11-08 16:10:09 +0530

    Call for papers page

---
+++ src/app/call/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Call = () => {
+    return (
+        <div>
+            <h1>Welcome to the Call for Papers Page</h1>
+        </div>
+    );
+};
+
+export default Call;
