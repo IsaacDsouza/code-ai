423961c446ec0e56c0ecfb1a2daeb2eebd441d32
IsaacDsouza <isaacmdsouza@gmail.com>
2024-11-08 16:07:10 +0530

    Speakers page

---
+++ src/app/speaker/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Speaker = () => {
+    return (
+        <div>
+            <h1>Welcome to the Speakers Page</h1>
+        </div>
+    );
+};
+
+export default Speaker;
