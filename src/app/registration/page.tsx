9ec961109fdff42efd898684616d10f2808ec4ef
IsaacDsouza <isaacmdsouza@gmail.com>
2024-11-08 16:06:22 +0530

    Registrations page

---
+++ src/app/registration/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Registration = () => {
+    return (
+        <div>
+            <h1>Welcome to the Registrations Page</h1>
+        </div>
+    );
+};
+
+export default Registration;
