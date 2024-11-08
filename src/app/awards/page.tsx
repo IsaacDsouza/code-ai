1db0f79440b3f6e7fe72562d8c3e1dba17edb38e
IsaacDsouza <isaacmdsouza@gmail.com>
2024-11-08 16:02:48 +0530

    Created pages

---
+++ src/app/awards/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Awards = () => {
+    return (
+        <div>
+            <h1>Welcome to the Awards Page</h1>
+        </div>
+    );
+};
+
+export default Awards;
