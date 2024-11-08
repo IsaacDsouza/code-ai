80feeb84747c3cf4d2d8188be65609f581b9a510
IsaacDsouza <isaacmdsouza@gmail.com>
2024-11-08 16:08:08 +0530

    Sponsorships page

---
+++ src/app/sponsorships/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sponsorship = () => {
+    return (
+        <div>
+            <h1>Welcome to the Sponsorships Page</h1>
+        </div>
+    );
+};
+
+export default Sponsorship;
